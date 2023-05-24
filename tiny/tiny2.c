53cfea620b006e26250af77347a68f6da07bd853
방우현 <bread@bang-uhyeon-ui-MacBookAir.local>
2023-05-24 10:28:39 +0900

    4th commit

--- tiny/tiny2.c
+++ tiny/tiny2.c
@@ -20,22 +20,22 @@ int main(int argc, char **argv)
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
-
+`
     /* Check command line args */
     if (argc != 2) {
 	fprintf(stderr, "usage: %s <port>\n", argv[0]);
 	exit(1);
     }
 
-    listenfd = Open_listenfd(argv[1]);
+    listenfd = Open_listenfd(argv[1]); //듣기 소켓을 오픈
     while (1) {
 	clientlen = sizeof(clientaddr);
-	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // line:netp:tiny:accept // 반복적으로 연결 요청을 접수하고
         Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
                     port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-	doit(connfd);                                             //line:netp:tiny:doit
-	Close(connfd);                                            //line:netp:tiny:close
+	doit(connfd);   // line:netp:tiny:doit //트랜잭션 시작
+	Close(connfd);   // line:netp:tiny:close //연결 끊음
     }
 }
 /* $end tinymain */
@@ -53,14 +53,14 @@ void doit(int fd)
     rio_t rio;
 
     /* Read request line and headers */
-    Rio_readinitb(&rio, fd);
+    Rio_readinitb(&rio, fd); //rio 구조체를 초기화 시킨다
     if (!Rio_readlineb(&rio, buf, MAXLINE))  //line:netp:doit:readrequest
-        return;
+        return; //요청헤드를 buf에 읽어온다
     printf("%s", buf);
-    sscanf(buf, "%s %s %s", method, uri, version);       //line:netp:doit:parserequest
+    sscanf(buf, "%s %s %s", method, uri, version); // 요청 헤더에서 메스드, url및 버전을 추출한다
     if (strcasecmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
         clienterror(fd, method, "501", "Not Implemented",
-                    "Tiny does not implement this method");
+                    "Tiny does not implement this method"); //404오류를 클라이언트에게 반환하고 함수를 종료합니다
         return;
     }                                                    //line:netp:doit:endrequesterr
     read_requesthdrs(&rio);                              //line:netp:doit:readrequesthdrs
@@ -74,7 +74,7 @@ void doit(int fd)
     }                                                    //line:netp:doit:endnotfound
 
     if (is_static) { /* Serve static content */          
-	if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //line:netp:doit:readable
+	if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //sbuf의 파일 모드가 일반파일이 아니거나 사용자에게 읽을 권한이 없는 경우
 	    clienterror(fd, filename, "403", "Forbidden",
 			"Tiny couldn't read the file");
 	    return;
@@ -115,28 +115,29 @@ void read_requesthdrs(rio_t *rp)
  *             return 0 if dynamic content, 1 if static
  */
 /* $begin parse_uri */
+/*doit 함수에서 호출되어 요청된 url를 파싱하여 파일 경로와 cgl인자를 추출하는 역할을 한다.*/
 int parse_uri(char *uri, char *filename, char *cgiargs) 
 {
     char *ptr;
 
     if (!strstr(uri, "cgi-bin")) {  /* Static content */ //line:netp:parseuri:isstatic
 	strcpy(cgiargs, "");                             //line:netp:parseuri:clearcgi
-	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert1
-	strcat(filename, uri);                           //line:netp:parseuri:endconvert1
+	strcpy(filename, ".");      // .로 초기화                     //line:netp:parseuri:beginconvert1
+	strcat(filename, uri);      // uri를 이어 붙인다.                     //line:netp:parseuri:endconvert1
 	if (uri[strlen(uri)-1] == '/')                   //line:netp:parseuri:slashcheck
-	    strcat(filename, "home.html");               //line:netp:parseuri:appenddefault
+	    strcat(filename, "home.html");  // 마지막 문자가 '/'면 home.html을 이어 붙인다.             //line:netp:parseuri:appenddefault
 	return 1;
     }
     else {  /* Dynamic content */                        //line:netp:parseuri:isdynamic
-	ptr = index(uri, '?');                           //line:netp:parseuri:beginextract
-	if (ptr) {
-	    strcpy(cgiargs, ptr+1);
-	    *ptr = '\0';
+	ptr = index(uri, '?');   // ?를 찾는다                        //line:netp:parseuri:beginextract
+	if (ptr) {// 문자를 찾는다면
+	    strcpy(cgiargs, ptr+1);// cgiargs에 ptr 다음 문자부터 끝까지의 문자열을 복사한다.
+	    *ptr = '\0'; //ptr \0로 설정하여 uri문자열을 종료한다.
 	}
 	else 
-	    strcpy(cgiargs, "");                         //line:netp:parseuri:endextract
+	    strcpy(cgiargs, "");    // 빈문자로 초기화 해버림                     //line:netp:parseuri:endextract
 	strcpy(filename, ".");                           //line:netp:parseuri:beginconvert2
-	strcat(filename, uri);                           //line:netp:parseuri:endconvert2
+	strcat(filename, uri);      // 이어 붙인다.                     //line:netp:parseuri:endconvert2
 	return 0;
     }
 }
@@ -153,6 +154,11 @@ void serve_static(int fd, char *filename, int filesize)
  
     /* Send response headers to client */
     get_filetype(filename, filetype);       //line:netp:servestatic:getfiletype
+    /*sprintf --> 함수는 형식화된 문자열을 생성하여 지정된 버퍼에 저장, 
+    생성된 문자열은 지정된 버퍼에 저장되기 때문에 특정 파일이나 화면에 직접 출력되지 않습니다.
+    대신, 문자열을 변수에 저장하거나 다른 함수로 전달하는 용도로 사용*/
+
+    /*client에게 헤더 전송*/
     sprintf(buf, "HTTP/1.0 200 OK\r\n");    //line:netp:servestatic:beginserve
     sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
     sprintf(buf, "%sConnection: close\r\n", buf);
